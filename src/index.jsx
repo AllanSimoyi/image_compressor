9f309bc1ae6781014c6b060789711e48cd82c705
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-06 10:52:03 +0200

    Fix: Update to new root client rendering API

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import ReactDOM from "react-dom";
-
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById( "root" );
+const root = createRoot( container );
+root.render( <App /> );
